62b7cf7d1bd188c5d9f651183e62ba7369fdb312
Assassin#1234 <chordsandfrets@gmail.com>
2021-06-19 00:05:16 +0530

    Update reroll.js

--- commands/reroll.js
+++ commands/reroll.js
@@ -1,5 +1,6 @@
 // require Nuggies
 const Nuggies = require('Nuggies');
+const  { MessageButton } = require('discord-buttons');
 module.exports.run = async (client, message, args) => {
     if (!args[0]) return message.reply('Please provide a message ID to the giveaway!', { allowedMentions: { repliedUser: false } });
     let win;
@@ -11,7 +12,7 @@ module.exports.run = async (client, message, args) => {
         return message.channel.send('Unable to find the giveaway!');
     }
     if (!win[0]) return message.channel.send('There are not enough people in the giveaway!');
-    message.channel.send(`Rerolled! ${win} is the new winner of the giveaway!`, { component: new MessageButton().setLabel('Giveaway').setURL(`https://discord.com/channels/${message.guild.id}/${message.channel.id}/${args[0]}`).setStyle('url') });
+    message.channel.send(`Rerolled! <@${win}> is the new winner of the giveaway!`, { component: new MessageButton().setLabel('Giveaway').setURL(`https://discord.com/channels/${message.guild.id}/${message.channel.id}/${args[0]}`).setStyle('url') });
 }
 
 module.exports.config = {
