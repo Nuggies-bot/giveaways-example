fa7a9521b5d39b6b0df2fdcc97ae7a93e2b8e7bf
Assassin#1234 <chordsandfrets@gmail.com>
2021-09-28 17:41:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ require('dotenv').config();
 const Discord = require('discord.js');
 // require Nuggies package
 const Nuggies = require('nuggies');
+// customizability
+const defaultGiveawayMessages = {
+	dmWinner: true,
+	giveaway: '🎉🎉 **GIVEAWAY!** 🎉🎉',
+	giveawayDescription: '🎁 Prize: **{prize}**\n🎊 Hosted by: {hostedBy}\n⏲️ Winner(s): `{winners}`\n\nRequirements: {requirements}',
+	endedGiveawayDescription : '🎁 Prize: **{prize}**\n🎊 Hosted by: {hostedBy}\n⏲️ Winner(s): {winners}',
+	giveawayFooterImage: 'https://cdn.discordapp.com/emojis/843076397345144863.png',
+	winMessage: 'congrats {winners}! you won `{prize}`!! Total `{totalParticipants}` members participated and your winning percentage was `{winPercentage}%`',
+	rerolledMessage: 'Rerolled! {winner} is the new winner of the giveaway!', // only {winner} placeholder
+	toParticipate: '**Click the Enter button to enter the giveaway!**',
+	newParticipant: 'You have successfully entered for this giveaway! your win percentage is `{winPercentage}%` among `{totalParticipants}` other participants', // no placeholders | ephemeral
+	alreadyParticipated: 'you already entered this giveaway!', // no placeholders | ephemeral
+	noParticipants: 'There are not enough people in the giveaway!', // no placeholders
+	noRole: 'You do not have the required role(s)\n{requiredRoles}\n for the giveaway!', // only {requiredRoles} | ephemeral
+	dmMessage: 'You have won a giveaway in **{guildName}**!\nPrize: [{prize}]({giveawayURL})',
+	noWinner: 'Not enough people participated in this giveaway.', // no {winner} placerholder
+	alreadyEnded: 'The giveaway has already ended!', // no {winner} placeholder
+	dropWin: '{winner} Won The Drop!!',
+};
 const client = new Discord.Client({ intents: 32767 });
 const fs = require('fs');
 Nuggies.giveaways.Messages(client, {})
@@ -19,6 +38,11 @@ client.on('ready', () => {
 // handle giveaway buttons
 Nuggies.handleInteractions(client)
 
+// setup custom messages
+
+Nuggies.Messages(bot, {
+	giveawayOptions: defaultGiveawayMessages,
+});
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
